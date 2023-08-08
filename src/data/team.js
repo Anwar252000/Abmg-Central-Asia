24ddbe132c9fa31935d6bf36c57ceb308d2f824d
Anwar Ul Islam <annu.anwar2000@gmail.com>
2023-08-08 17:57:40 +0500

    changes

--- src/data/team.js
+++ src/data/team.js
@@ -4,15 +4,16 @@ const teams = [
 		img: '/img/team/t1.jpg',
 		name: 'Ts. Dr. Liew Taik Hwa',
 		info: 'Director of ABM Group & KEC Group President for The Beacon Society (NGO)',
-		experience: '14 Years of Experience',
+		experience: '20 Years of working exp',
 		achievement:
 			'2015 SMERA Rising Star Award, 2016 Platinum Business Award, 2019 Platinum Business Award',
 	},
 	{
 		id: 2,
 		img: '/img/team/t2.jpg',
-		name: 'Salman Ahsan',
-		info: 'CEO of Pak Premier Hospitality  SDN BHD Malaysia.',
+		name: 'Mr. Salman Ahsan',
+		experience: '20 Years of working exp',
+		info: 'Msc (ECO), Bsc (Hon) Commerce.',
 	},
 ];
 
